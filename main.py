b160e51e47bf597800989bf3209d354970a255dd
Christian Berwanger <christian.berwanger@rwth-aachen.de>
2024-06-20 15:25:18 +0200

    First Analysis

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import neurokit2 as nk
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Parameters
+samplingRatePre = 50  # In Hz
+samplingRateDuring = 50  # In Hz
+# import CSV as dataframe
+rawdata = pd.read_csv('data/christianTW.csv', sep=';', decimal=',')
+
+# Extract the ECG signal
+ecg_signal = rawdata["Lauf 1: Potential (mV)"].values
+cleanECG = nk.ecg_clean(ecg_signal, sampling_rate=samplingRatePre)
+
+# Peak detection and basic analysis
+peaks, info = nk.ecg_peaks(cleanECG, sampling_rate=samplingRatePre)
+hrv = nk.hrv(peaks, sampling_rate=samplingRatePre)
+
+# Get heart rate
+heart_rate = nk.ecg_rate(peaks, sampling_rate=samplingRatePre)
+# Extract the numerical value of the heart rate from the array
+average_heart_rate = heart_rate[:].mean()
+peak_times = info['ECG_R_Peaks'] / samplingRatePre
+# Now you can use the f-string for formatting
+print(f"Average Heart Rate: {average_heart_rate:.2f} bpm")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+# Get time points of R-peaks
+r_peaks_time = peaks['ECG_R_Peaks'] / samplingRatePre
 
+# Plot the cleaned ECG signal and the detected R-peaks
+#nk.events_plot(r_peaks_time, cleanECG)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Plot HRV analysis (you can explore different HRV metrics)
+#nk.hrv_plot(hrv)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+HeartRateData = pd.data = {'Time (seconds)': peak_times, 'Average Heart Rate (bpm)': heart_rate}
+heartrate = pd.DataFrame(HeartRateData)
+plt.figure()
+sns.lineplot(x='Lauf 1: Zeit (min)', y='Lauf 1: Potential (mV)', data=rawdata, marker='x')
+
+plt.figure()
+sns.set_theme(style="whitegrid")
+sns.lineplot(x='Time (seconds)', y='Average Heart Rate (bpm)', data=heartrate, marker='o')
+plt.title('Average Heart Rate Over Time')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Average Heart Rate (bpm)')
+plt.show()
